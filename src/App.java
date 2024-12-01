86754dacc37fa150cfc6a41ee2b9841e0fce32be
Sankalp Bhagwat <sankalp_bhagwat@epam.com>
2024-12-01 13:15:09 +0530

    Fix scanner closing logic

--- src/App.java
+++ src/App.java
@@ -4,19 +4,27 @@ import entities.ParkingSpot;
 import entities.Vehicle;
 import enums.ParkingSpotType;
 import enums.VehicleType;
+import utilities.payment.PaymentModeSelector;
 
 public class App {
     public static void main(String[] args) throws Exception {
         ParkingLot parkingLot = new ParkingLot(new ParkingAgent("Sankalp", "1"));
-        Vehicle vehicle1 = new Vehicle(VehicleType.CAR, "Vehicle1");
-        Vehicle vehicle2 = new Vehicle(VehicleType.CAR, "Vehicle2");
 
         parkingLot.addParkingSpot(new ParkingSpot(ParkingSpotType.CAR));
         parkingLot.addParkingSpot(new ParkingSpot(ParkingSpotType.CAR));
+
+        Vehicle vehicle1 = new Vehicle(VehicleType.CAR, "Vehicle1");
+        Vehicle vehicle2 = new Vehicle(VehicleType.CAR, "Vehicle2");
 
         parkingLot.allocateParking(vehicle1);
         parkingLot.allocateParking(vehicle2);
 
         parkingLot.exitParking(vehicle1);
+
+        System.out.println("*********VEHICLE 1 HAS EXITED***********");
+
+        parkingLot.exitParking(vehicle2);
+
+        PaymentModeSelector.closeScanner();
     }
 }
--- src/utilities/payment/PaymentModeSelector.java
+++ src/utilities/payment/PaymentModeSelector.java
@@ -8,17 +8,18 @@ import interfaces.PaymentMode;
 
 public class PaymentModeSelector {
     private static Map<Integer, PaymentMode> paymentModes;
+    private static Scanner scanner;
 
     static {
         paymentModes = new HashMap<>();
         paymentModes.put(1, new CashPayment());
         paymentModes.put(2, new UPIPayment());
         paymentModes.put(3, new CardPayment());
+
+        scanner = new Scanner(System.in);
     }
 
     public static PaymentMode selectPaymentMode() {
-        Scanner scanner = new Scanner(System.in);
-
         for (Map.Entry<Integer, PaymentMode> entry : paymentModes.entrySet()) {
             System.out.println(
                     entry.getKey() + ". " + entry.getValue().getClass().getSimpleName().replace("Payment", ""));
@@ -34,9 +35,11 @@ public class PaymentModeSelector {
             } catch (Exception e) {
                 System.out.println("Invalid input. Please enter an integer.");
                 scanner.nextLine();
-            } finally {
-                scanner.close();
             }
         }
+    }
+
+    public static void closeScanner() {
+        scanner.close();
     }
 }
